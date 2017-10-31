33fb4c94e57eb6a5fe984573dd117a8642fcfe77
Andrew Crabb <andrew@crabb.org>
2017-10-31 17:52:10 -0400

    setup.sh add -D _GLIBCXX_USE_CXX11_ABI=0 to turn off C++11 symbols.  libscatter.a uses C++03 symbols

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mypwd=`pwd`
-export MYCPPFLAGS="-D_LINUX -D__LINUX__ -I${mypwd}/AIR5.3.0/src -L${mypwd}/AIR5.3.0/src"
+# Note -D _GLIBCXX_USE_CXX11_ABI=0 is necessary to turn off C++11 symbols.
+# libscatter.a uses C++03 symbols and can't resolve Logging::logMsg if progs are compiled with C++11
+export MYCPPFLAGS="-D_LINUX -D__LINUX__ -D _GLIBCXX_USE_CXX11_ABI=0 -I${mypwd}/AIR5.3.0/src -L${mypwd}/AIR5.3.0/src"
 # export MYCPPFLAGS="-D_LINUX -D__LINUX__ -D__MACOSX__ -I${mypwd}/AIR5.3.0/src -L${mypwd}/AIR5.3.0/src"
 export MYCFLAGS='-g'
