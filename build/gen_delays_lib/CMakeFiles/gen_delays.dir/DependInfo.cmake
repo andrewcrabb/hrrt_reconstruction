0aa00046c9b804d0a8d6f3aa245ab94196097b91
Andrew Crabb <ahc@jhu.edu>
2017-11-10 18:31:05 -0500

    Baby steps.  First CMakeLists files in place, building gen_delays_lib

---
+++ build/gen_delays_lib/CMakeFiles/gen_delays.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ahc/DEV/hrrt_open_2011/gen_delays_lib/gen_delays.cpp" "/Users/ahc/DEV/hrrt_open_2011/build/gen_delays_lib/CMakeFiles/gen_delays.dir/gen_delays.cpp.o"
+  "/Users/ahc/DEV/hrrt_open_2011/gen_delays_lib/geometry_info.cpp" "/Users/ahc/DEV/hrrt_open_2011/build/gen_delays_lib/CMakeFiles/gen_delays.dir/geometry_info.cpp.o"
+  "/Users/ahc/DEV/hrrt_open_2011/gen_delays_lib/lor_sinogram_map.cpp" "/Users/ahc/DEV/hrrt_open_2011/build/gen_delays_lib/CMakeFiles/gen_delays.dir/lor_sinogram_map.cpp.o"
+  "/Users/ahc/DEV/hrrt_open_2011/gen_delays_lib/segment_info.cpp" "/Users/ahc/DEV/hrrt_open_2011/build/gen_delays_lib/CMakeFiles/gen_delays.dir/segment_info.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=0"
+  "_LINUX"
+  "__LINUX__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../gen_delays_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
